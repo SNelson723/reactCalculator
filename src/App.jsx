c52741f40d9d5be8cdb1e55deba53f28a111aafa
SNelson723 <stephenn649@gmail.com>
2025-01-07 23:15:42 -0600

    Added react-icons for the symbols and started working on the calculator grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@
 const App = () => {
 
   return (
-    <div>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">
 
+        </div>
+
+        <div className="current-operand">
+
+        </div>
+      </div>
+
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>DEL</button>
     </div>
 
   )
 }
 
-export default App
+export default App;
