2828d2488780df5bf5f14705212d5a0387e38cc6
SNelson723 <stephenn649@gmail.com>
2025-01-07 23:55:44 -0600

    Changed some eslint rules and replaced the original digit buttons with the DigitButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import DigitButton from "./DigitButton";
 
 export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
@@ -31,20 +32,20 @@ const App = () => {
       <button className="span-two">AC</button>
       <button>DEL</button>
       <button>&divide;</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <DigitButton digit='1' dispatch={dispatch} />
+      <DigitButton digit='2' dispatch={dispatch} />
+      <DigitButton digit='3' dispatch={dispatch} />
       <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
+      <DigitButton digit='4' dispatch={dispatch} />
+      <DigitButton digit='5' dispatch={dispatch} />
+      <DigitButton digit='6' dispatch={dispatch} />
       <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <DigitButton digit='7' dispatch={dispatch} />
+      <DigitButton digit='8' dispatch={dispatch} />
+      <DigitButton digit='9' dispatch={dispatch} />
       <button>-</button>
       <button>.</button>
-      <button>0</button>
+      <DigitButton digit='0' dispatch={dispatch} />
       <button className="span-two">=</button>
 
     </div>
