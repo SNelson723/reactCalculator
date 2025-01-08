2f68f7fdb522476088bd1a881d4220d4057a4f8b
SNelson723 <stephenn649@gmail.com>
2025-01-07 23:19:21 -0600

    Added all the buttons with the correct numbers and symbols

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,22 @@ const App = () => {
       <button className="span-two">AC</button>
       <button>DEL</button>
       <button><CgMathDivide /></button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+
     </div>
 
   )
