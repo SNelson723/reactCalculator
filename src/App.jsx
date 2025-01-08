3e4178928afa932d1acbf4d5c7e98f66c5512eb7
SNelson723 <stephenn649@gmail.com>
2025-01-08 00:38:48 -0600

    Finished the project and calculator is working

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,14 @@ export const ACTIONS = {
 const reducer = (state, { type, payload }) => {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false
+        }
+      }
+
       if (payload.digit === '0' && state.currentOperand === '0') return state;
       if (payload.digit === '.' && state.currentOperand.includes('.')) return state;
 
@@ -25,8 +33,16 @@ const reducer = (state, { type, payload }) => {
       return {};
     case ACTIONS.CHOOSE_OPERATION:
       // if nothing was clicked
-      
       if (state.currentOperand == null && state.previousOperand == null) return state;
+
+      // if you need to switch operands mid evaluation
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation
+        }
+      }
+
       // if the current operand is not null
       if (state.previousOperand == null) {
         return {
@@ -43,6 +59,38 @@ const reducer = (state, { type, payload }) => {
         previousOperand: evaluate(state),
         currentOperand: null,
         operation: payload.operation
+      }
+    // calculate the operation
+    case ACTIONS.EVALUATE:
+      if (state.operation == null || state.currentOperand == null || state.operation == null) {
+        return state;
+      }
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        currentOperand: evaluate(state),
+        operation: null
+      }
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null
+        }
+      }
+      if (state.currentOperand == null) return state;
+      // if only one digit is left
+      if (state.currentOperand.length === 1) {
+        return {...state, currentOperand: null}
+      }
+
+      // default behavior
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
       }
   }
 };
@@ -68,6 +116,25 @@ const evaluate = ({ currentOperand, previousOperand, operation }) => {
       computation = prev / current;
       break;
   }
+
+  return computation.toString();
+};
+
+const integerFormatter = new Intl.NumberFormat('en-us', {
+  maximumFractionDigits: 0
+});
+
+const formatOperand = (operand) => {
+  // if there is nothing to format
+  if (operand == null) return;
+
+  const [integer, decimal] = operand.split('.');
+
+  // if there is no decimal
+  if (decimal == null) return integerFormatter.format(integer);
+
+  // if there is a decimal
+  return `${integerFormatter.format(integer)}.${decimal}`
 };
 
 const App = () => {
@@ -76,12 +143,12 @@ const App = () => {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">{previousOperand} {operation}</div>
-        <div className="current-operand">{currentOperand}</div>
+        <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
 
       <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
 
       <OperationButton operation='÷' dispatch={dispatch} />
       <DigitButton digit='1' dispatch={dispatch} />
@@ -101,7 +168,7 @@ const App = () => {
       <OperationButton operation='-' dispatch={dispatch} />
       <DigitButton digit='.' dispatch={dispatch} />
       <DigitButton digit='0' dispatch={dispatch} />
-      <button className="span-two">=</button>
+      <button className="span-two"  onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</button>
 
     </div>
 
